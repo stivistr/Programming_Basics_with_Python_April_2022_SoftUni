346d9e563dd93c439708268fc2b11d9cc871b04a
stivistr <stivan_strahilov@abv.bg>
2022-12-20 20:58:41 +0200

    Basics of Python

---
+++ pet_shop.py
@@ -0,0 +1,7 @@
+number_of_packs_for_dog_food = int(input())
+number_of_packs_for_cat_food = int(input())
+price_for_all_packs_for_dog_foot = number_of_packs_for_dog_food * 2.50
+price_for_all_packs_for_cat_foot = number_of_packs_for_cat_food * 4
+final_sum = price_for_all_packs_for_dog_foot + price_for_all_packs_for_cat_foot
+
+print(f'{final_sum} lv.')
